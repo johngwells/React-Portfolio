4ed27998edd6af2fbef65b8d36ff4a163ffee0c1
John Wells <molekewl2017@gmail.com>
2019-08-17 00:39:45 -0700

    show more - animation & unmounts when you show less

--- src/Title.js
+++ src/Title.js
@@ -8,14 +8,16 @@ const TITLES = [
 ];
 
 class Title extends Component {
-  state = { titleIndex: 0 };
+  state = { titleIndex: 0, fadeIn: true };
 
   componentDidMount() {
+    this.timeout = setTimeout(() => this.setState({ fadeIn: false}), 2000);
     this.animateTitles();
   }
 
   componentWillUnmount() {
     clearInterval(this.titleInterval);
+    clearTimeout(this.timeout);
   }
 
   animateTitles = () => {
@@ -24,15 +26,19 @@ class Title extends Component {
       // because the index exceeds the length returning undefined.
       const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
 
-      this.setState({ titleIndex });
+      this.setState({ titleIndex, fadeIn: true });
+
+      this.timeout = setTimeout(() => this.setState({ fadeIn: false}), 2000);
     }, 4000);
   }
 
   render() {
-    const title = TITLES[this.state.titleIndex]
+    const { fadeIn, titleIndex } = this.state;
+
+    const title = TITLES[titleIndex]
 
     return (
-      <p>I am {title}</p>
+      <p className={fadeIn ? 'title-fade-in' : 'title-fade-out'}>I am {title}</p>
     )
   }
 }
