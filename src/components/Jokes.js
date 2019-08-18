6bbcc9666b95b9305f036489667996198903ea77
John Wells <molekewl2017@gmail.com>
2019-08-17 22:25:53 -0700

    user can see 10 jokes by clicking

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class Jokes extends Component {
-  state = { joke: {} };
+  state = { joke: {}, jokes: [] };
 
   componentDidMount() {
     fetch('https://official-joke-api.appspot.com/random_joke')
       .then(response => response.json())
       .then(json => this.setState({ joke: json }));
+  }
+
+  fetchJokes = () => {
+    fetch('https://official-joke-api.appspot.com/random_ten')
+      .then(response => response.json())
+      .then(json => this.setState({ jokes: json }));
   }
 
   render() {
@@ -15,6 +21,16 @@ class Jokes extends Component {
       <div>
         {/* <h5>Highlighted Joke</h5> */}
         <p>(Joke Api) - {setup} <em>{punchline}</em></p>
+        <hr />
+        <h3>Want ten new jokes?</h3>
+        <button onClick={this.fetchJokes}>Click me!</button>
+        {
+          this.state.jokes.map(joke => {
+            const { id, setup, punchline } = joke;
+
+            return <p key={id}>{setup} <em>{punchline}</em></p>
+          })
+        }
       </div>
     )
   }
